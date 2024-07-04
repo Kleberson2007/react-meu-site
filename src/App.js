3b2e51faf9108c54d5c37b00cf9db7f1a081dbb0
Kleberson dos Santos Mendes Pinto <klebersonsantosmp@gmail.com>
2024-07-04 15:01:04 -0300

    s

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default function App() {
   return (
     <section>
-      <div className='apresentação'>
+      <div class='apresentação'>
         <h1 className='titulo'>Olá, eu sou kleberson</h1>
         <p className='texto'>Nascido em 02/05/2007, em Brasilia de Minas-MG. sou um novo estudante na ELITI, uma escola excelente de programadores. Ainda sou novato com progração, tendo recentemente completado um mês na ELITI. ainda sou meio inexperiente, tendo começado agora no react, mas estou me esforçando para melhorar. atualmente tenho 17 anos, busco me tornar um profissional em programação e conseguir atingir grandes conquistas nesta carreira. </p>
         <p className='texto'>Aqui estão alguns de meus projetos:</p>
@@ -13,7 +13,7 @@ export default function App() {
         </section>
       </div>
       <div className='imagem'>
-        <img src='eu.JPG' rel='eu' width="450px" height="300px" />
+        <img src='teste.png' rel='eu' width="400px" height="600px" />
       </div>
     </section>
   );
